6970870ba83358be699d8db6bbbfd3ca6f5fdfdc
Mike McGowan <mike.mcgowan@getbridge.com>
2022-11-17 11:53:51 +0000

    6.2, 6.3

--- src/main/kotlin/chapter6/exercises.kt
+++ src/main/kotlin/chapter6/exercises.kt
@@ -20,3 +20,28 @@ fun nonNegativeInt(rng: RNG): Pair<Int, RNG> {
     val result = if (n >= 0) n else if (n == Int.MIN_VALUE) 0 else abs(n)
     return result to rng2
 }
+
+fun double(rng: RNG): Pair<Double, RNG> {
+    val (result, rng2) = nonNegativeInt(rng)
+    val x = if (result == Int.MAX_VALUE) result - 1 else result
+    return (x.toDouble() / Int.MAX_VALUE.toDouble()) to rng2
+}
+
+fun intDouble(rng: RNG): Pair<Pair<Int, Double>, RNG> {
+    val (i, rng2) = nonNegativeInt(rng)
+    val (d, rng3) = double(rng2)
+    return (i to d) to rng3
+}
+
+fun doubleInt(rng: RNG): Pair<Pair<Double, Int>, RNG> {
+    val (d, rng2) = double(rng)
+    val (i, rng3) = nonNegativeInt(rng2)
+    return (d to i) to rng3
+}
+
+fun double3(rng: RNG): Pair<Triple<Double, Double, Double>, RNG> {
+    val (d1, rng2) = double(rng)
+    val (d2, rng3) = double(rng2)
+    val (d3, rng4) = double(rng3)
+    return Triple(d1, d2, d3) to rng4
+}
--- src/test/kotlin/chapter6/Chapter6Exercises.kt
+++ src/test/kotlin/chapter6/Chapter6Exercises.kt
@@ -2,13 +2,38 @@ package chapter6
 
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 internal class Chapter6Exercises {
 
+    private val rng = SimpleRNG(42)
+
     @Test
     fun exercise6dot1() {
-        val rng = SimpleRNG(42)
         val (n, _) = nonNegativeInt(rng)
         assertEquals(16159453, n)
+    }
+
+    @Test
+    fun exercise6dot2() {
+        val (d, _) = double(rng)
+        assertTrue(d > 0.00751 && d < 0.00753)
+    }
+
+    @Test
+    fun exercise6dot3() {
+        val (p1, _) = intDouble(rng)
+        assertEquals(16159453, p1.first)
+        assertTrue(p1.second > 0.59672 && p1.second < 0.59674)
+
+        val (p2, _) = doubleInt(rng)
+        assertTrue(p2.first > 0.00751 && p2.first < 0.00753)
+        assertEquals(1281479697, p2.second)
+
+        val (t, _) = double3(rng)
+        println(t)
+        assertTrue(t.first > 0.00751 && t.first < 0.00753)
+        assertTrue(t.second > 0.59672 && t.second < 0.59674)
+        assertTrue(t.third > 0.15845 && t.third < 0.15847)
     }
 }
