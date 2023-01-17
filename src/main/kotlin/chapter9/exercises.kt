7adc2a30c160f870cb69772e0ecd474591cc9ec4
Mike McGowan <mike.mcgowan@getbridge.com>
2023-01-17 12:54:19 +0000

    9.4

--- src/main/kotlin/chapter9/exercises.kt
+++ src/main/kotlin/chapter9/exercises.kt
@@ -16,7 +16,7 @@ interface Parsers<PE> {
     fun <A> Parser<A>.many(): Parser<List<A>>
     fun <A> Parser<A>.many1(): Parser<List<A>>
     fun <A, B> Parser<A>.map(f: (A) -> B): Parser<B>
-    fun <A, B, C> map2(pa: Parser<A>, pb: () -> Parser<B>, f: (A, B) -> C): Parser<C>
+    fun <A, B, C> map2(pa: Parser<A>, pb: Parser<B>, f: (A, B) -> C): Parser<C>
     infix fun <A, B> Parser<A>.product(p: Parser<B>): Parser<Pair<A, B>>
 }
 
@@ -24,12 +24,18 @@ object ParseError
 
 abstract class Foo : Parsers<ParseError> {
 
-    override fun <A, B, C> map2(pa: Parser<A>, pb: () -> Parser<B>, f: (A, B) -> C): Parser<C> =
-        pa.product(pb()).map { (a, b) -> f(a, b) }
+    override fun <A, B, C> map2(pa: Parser<A>, pb: Parser<B>, f: (A, B) -> C): Parser<C> =
+        pa.product(pb).map { (a, b) -> f(a, b) }
 
     override fun <A> Parser<A>.many1(): Parser<List<A>> =
-        map2(this, { many() }) { a, b -> listOf(a) + b }
+        map2(this, many()) { a, b -> listOf(a) + b }
 
     override fun <A> Parser<A>.many(): Parser<List<A>> =
-        map2(this, { many() }) { a, b -> listOf(a) + b } or succeed(emptyList())
+        map2(this, many()) { a, b -> listOf(a) + b } or succeed(emptyList())
+
+    override fun <A> listOfN(n: Int, p: Parser<A>): Parser<List<A>> =
+        when {
+            n > 0 -> map2(p, listOfN(n - 1, p)) { a, b -> listOf(a) + b }
+            else -> succeed(emptyList())
+        }
 }
